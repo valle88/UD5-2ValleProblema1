1538bf848a220b532453fa131f959cb919066fdd
Valle Deniz Diaz <vallukis@gmail.com>
2022-05-26 10:18:12 +0100

    feacture: subir arreglos

--- src/SuperList.java
+++ src/SuperList.java
@@ -1,19 +1,24 @@
 public class SuperList extends  ListManager <T>{
-
+    private final int TOP= 0;
 
     @Override
     public void addElement(T element) {
+        list.add(TOP, element);
 
     }
 
     @Override
-    public t removeElement(int position) {
-        return null;
+    public T removeElement(int position) {
+        return list.remove(TOP);
     }
 
     @Override
     public void showElements() {
+        for (int i = 0; i < list.size(); i++) {
+            int valor = list.get(i);
+            System.out.println("elemento  " +(i+1 ) +" : " + valor);
 
+        }
     }
 
     @Override
