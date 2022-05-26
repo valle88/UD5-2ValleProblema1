eada7c4c155cbba1c705224862f119ed750dc377
Valle Deniz Diaz <vallukis@gmail.com>
2022-05-26 09:47:52 +0100

    crear las dos clases

---
+++ src/SuperList.java
@@ -0,0 +1,23 @@
+public class SuperList extends  ListManager <T>{
+
+
+    @Override
+    public void addElement(T element) {
+
+    }
+
+    @Override
+    public t removeElement(int position) {
+        return null;
+    }
+
+    @Override
+    public void showElements() {
+
+    }
+
+    @Override
+    public void showReversedElements() {
+
+    }
+}
